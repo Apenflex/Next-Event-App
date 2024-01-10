03a8d0b3a563b1b9847a6366d692348b666b35c3
Svyatoslav <blackdbf@gmail.com>
2024-01-10 12:45:29 +0200

    fix bug with username

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
 	const eventType = evt.type;
 
 	if (eventType === 'user.created') {
-		const { id, email_addresses, image_url, first_name, last_name } = evt.data;
+		const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
 		const user = {
 			clerkId: id,
